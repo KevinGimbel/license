e8b560198c157deb1f9287bc95ebdbfcf3ecedac
Kevin Gimbel <hallo@kevingimbel.com>
2017-11-06 12:02:24 +0100

    tools: Run go fmt

--- magefile.go
+++ magefile.go
@@ -17,7 +17,7 @@ func flagEnv() map[string]string {
 	return map[string]string{
 		"PACKAGE":    packageName,
 		"BUILD_DATE": time.Now().Format("2006-01-02T15:04:05Z0700"),
-		"VERSION": "1.0.0",
+		"VERSION":    "1.0.0",
 	}
 }
 
